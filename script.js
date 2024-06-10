b50b6df4e0a5e2a12a4da7c6b90335dc72141631
Zuzana Benkova <zuzanka.dvorska@gmail.com>
2024-06-11 00:15:31 +0200

    nove soubory

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const
+//Obsah elipsy
+const ellipseArea = (height, width) => {
+    let vysledek=height/2*width/2*Math.PI
+    return vysledek
+}
+
+ellipseArea (29,30)
+
+document.body.innerHTML += ellipseArea (1, 2)
+
+
+// maximum ze dvou cisel
+
+const max2 = (number1, number2) => {
+    let vysledek 
+    if (number1 <= number2) {
+        vysledek = number2
+    } else {
+        vysledek = number1
+    } 
+    return vysledek
+}
+max2 (2,3)
+
+document.body.innerHTML += max2(678, 76)
